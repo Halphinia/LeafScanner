0aa485f30a2c5f545392ef6a57fe8781f5477149
Hal <h.forbis2018@gmail.com>
2025-09-19 16:21:25 -0500

    added more flask framework stuff

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hehehehe</h1>'
+    return render_template('test.html')
+
+
+@app.route('/home')
+def home():
+    return '<h1>Heh kasdhfkljsahdflkjashflkjsahfdsakjlhfd</h1>'
+
+@app.route('/about')
+def aboutPage():
+    return render_template('about.html')
+
+@app.route('/upload')
+def uploadPage():
+    return render_template('upload.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ setup.md
@@ -0,0 +1,8 @@
+Windows: run $env:FLASK_APP = "main"
+Linux: run export FLASK_APP=main.py
+
+### Debug Mode
+set FLASK_DEBUG=1
+
+flask --help
+flask run
