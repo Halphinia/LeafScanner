5f7f892b301169c00e0edfa40aedc19914a86a82
Hal <h.forbis2018@gmail.com>
2025-09-26 22:14:22 -0500

    minor changes

--- main.py
+++ main.py
@@ -6,17 +6,13 @@ app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 @app.route('/')
+@app.route('/home')
 def index():
     return render_template('test.html')
 
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
-
-
-@app.route('/home')
-def home():
-    return '<h1>Heh kasdhfkljsahdflkjashflkjsahfdsakjlhfd</h1>'
 
 @app.route('/about')
 def aboutPage():
@@ -36,7 +32,6 @@ def uploadPage():
         return render_template("processing.html")
     else:
         return render_template('upload.html')
-
 
 if __name__ == "__main__":
     app.run(debug=True)
